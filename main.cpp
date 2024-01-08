950392da182e56e825354f2730c67288a733be50
charles-ison <isonc@oregonstate.edu>
2024-01-07 16:22:39 -0800

    Adding opengl example.

--- main.cpp
+++ main.cpp
@@ -1,126 +1,30 @@
-#include "scenemodifier.h"
-#include <Qt3DRender/qcamera.h>
-#include <Qt3DCore/qentity.h>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QWidget>
-#include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QCheckBox>
-#include <QtWidgets/QCommandLinkButton>
-#include <QtGui/QScreen>
-#include <Qt3DRender/qtexture.h>
-#include <Qt3DRender/qrenderpass.h>
-#include <Qt3DRender/qsceneloader.h>
-#include <Qt3DRender/qpointlight.h>
-#include <Qt3DCore/qtransform.h>
-#include <Qt3DCore/qaspectengine.h>
-#include <Qt3DRender/qrenderaspect.h>
-#include <Qt3DExtras/qforwardrenderer.h>
-#include <Qt3DExtras/qt3dwindow.h>
-#include <Qt3DExtras/qfirstpersoncameracontroller.h>
+// Copyright (C) 2016 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-int main(int argc, char **argv)
+#include <QApplication>
+#include <QLabel>
+#include <QSurfaceFormat>
+
+#ifndef QT_NO_OPENGL
+#include "mainwidget.h"
+#endif
+
+int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    Qt3DExtras::Qt3DWindow *view = new Qt3DExtras::Qt3DWindow();
-    view->defaultFrameGraph()->setClearColor(QColor(QRgb(0x4d4d4f)));
-    QWidget *container = QWidget::createWindowContainer(view);
-    QSize screenSize = view->screen()->size();
-    container->setMinimumSize(QSize(200, 100));
-    container->setMaximumSize(screenSize);
 
-    QWidget *widget = new QWidget;
-    QHBoxLayout *hLayout = new QHBoxLayout(widget);
-    QVBoxLayout *vLayout = new QVBoxLayout();
-    vLayout->setAlignment(Qt::AlignTop);
-    hLayout->addWidget(container, 1);
-    hLayout->addLayout(vLayout);
+    QSurfaceFormat format;
+    format.setDepthBufferSize(24);
+    QSurfaceFormat::setDefaultFormat(format);
 
-    widget->setWindowTitle(QStringLiteral("Basic shapes"));
-
-    // Root entity
-    Qt3DCore::QEntity *rootEntity = new Qt3DCore::QEntity();
-
-    // Camera
-    Qt3DRender::QCamera *cameraEntity = view->camera();
-
-    cameraEntity->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
-    cameraEntity->setPosition(QVector3D(0, 0, 20.0f));
-    cameraEntity->setUpVector(QVector3D(0, 1, 0));
-    cameraEntity->setViewCenter(QVector3D(0, 0, 0));
-
-    Qt3DCore::QEntity *lightEntity = new Qt3DCore::QEntity(rootEntity);
-    Qt3DRender::QPointLight *light = new Qt3DRender::QPointLight(lightEntity);
-    light->setColor("white");
-    light->setIntensity(1);
-    lightEntity->addComponent(light);
-    Qt3DCore::QTransform *lightTransform = new Qt3DCore::QTransform(lightEntity);
-    lightTransform->setTranslation(cameraEntity->position());
-    lightEntity->addComponent(lightTransform);
-
-    // For camera controls
-    Qt3DExtras::QFirstPersonCameraController *camController = new Qt3DExtras::QFirstPersonCameraController(rootEntity);
-    camController->setCamera(cameraEntity);
-
-    // Scenemodifier
-    SceneModifier *modifier = new SceneModifier(rootEntity);
-
-    // Set root object of the scene
-    view->setRootEntity(rootEntity);
-
-    // Create control widgets
-    QCommandLinkButton *info = new QCommandLinkButton();
-    info->setText(QStringLiteral("Qt3D ready-made meshes"));
-    info->setDescription(QString::fromLatin1("Qt3D provides several ready-made meshes, like torus, cylinder, "
-                                             "cube, plane and sphere."));
-    info->setIconSize(QSize(0,0));
-
-    QCheckBox *torusCB = new QCheckBox(widget);
-    torusCB->setChecked(true);
-    torusCB->setText(QStringLiteral("Torus"));
-
-    QCheckBox *cylinderCB = new QCheckBox(widget);
-    cylinderCB->setChecked(true);
-    cylinderCB->setText(QStringLiteral("Cylinder"));
-
-    QCheckBox *cuboidCB = new QCheckBox(widget);
-    cuboidCB->setChecked(true);
-    cuboidCB->setText(QStringLiteral("Cuboid"));
-
-    QCheckBox *planeCB = new QCheckBox(widget);
-    planeCB->setChecked(true);
-    planeCB->setText(QStringLiteral("Plane"));
-
-    QCheckBox *sphereCB = new QCheckBox(widget);
-    sphereCB->setChecked(true);
-    sphereCB->setText(QStringLiteral("Sphere"));
-
-    vLayout->addWidget(info);
-    vLayout->addWidget(torusCB);
-    vLayout->addWidget(cylinderCB);
-    vLayout->addWidget(cuboidCB);
-    vLayout->addWidget(planeCB);
-    vLayout->addWidget(sphereCB);
-
-    QObject::connect(torusCB, &QCheckBox::stateChanged,
-                     modifier, &SceneModifier::enableTorus);
-    QObject::connect(cylinderCB, &QCheckBox::stateChanged,
-                     modifier, &SceneModifier::enableCylinder);
-    QObject::connect(cuboidCB, &QCheckBox::stateChanged,
-                     modifier, &SceneModifier::enableCuboid);
-    QObject::connect(planeCB, &QCheckBox::stateChanged,
-                     modifier, &SceneModifier::enablePlane);
-    QObject::connect(sphereCB, &QCheckBox::stateChanged,
-                     modifier, &SceneModifier::enableSphere);
-
-    torusCB->setChecked(true);
-    cylinderCB->setChecked(true);
-    cuboidCB->setChecked(true);
-    planeCB->setChecked(true);
-    sphereCB->setChecked(true);
-
-    // Show window
-    widget->show();
-    widget->resize(1200, 800);
-
+    app.setApplicationName("cube");
+    app.setApplicationVersion("0.1");
+#ifndef QT_NO_OPENGL
+    MainWidget widget;
+    widget.show();
+#else
+    QLabel note("OpenGL Support required");
+    note.show();
+#endif
     return app.exec();
 }
