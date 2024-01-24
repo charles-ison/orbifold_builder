c1ae41c84729d46195de7f68fbe916df6a83fecd
charles-ison <isonc@oregonstate.edu>
2024-01-24 01:48:41 -0800

    Changing distance threshold for loop detection.

--- ResultsWidget.cpp
+++ ResultsWidget.cpp
@@ -83,7 +83,7 @@ void ResultsWidget::checkLineVerticesForLoop(QVector3D newVertexPosition) {
     for (int i=0; i<=lineVertices.size()-numRecentVerticesToExcludeForLoopChecking; i++) {
         QVector3D oldVertexPosition = lineVertices[i].position;
         float distance = sqrt(pow(oldVertexPosition.x() - newVertexPosition.x(), 2) + pow(oldVertexPosition.y() - newVertexPosition.y(), 2) + pow(oldVertexPosition.z() - newVertexPosition.z(), 2));
-        if (distance < 0.01) {
+        if (distance < 0.02) {
             isDrawingMode = false;
             for (int j=0; j<i; j++) {
                 lineVertices.erase(lineVertices.begin());
