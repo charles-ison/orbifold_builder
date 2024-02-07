b0f7f771d2afc54244886364749d49983250af40
charles-ison <isonc@oregonstate.edu>
2024-02-06 20:41:21 -0800

    Adding some alternative cross-cap parametrizations.

--- surfaces/CrossCap.cpp
+++ surfaces/CrossCap.cpp
@@ -8,17 +8,31 @@ CrossCap::CrossCap() {
 }
 
 void CrossCap::initVertices() {
-    float horStepSize = 2 * M_PI / numHorSteps;
-    float vertStepSize = M_PI / (2 * numVertSteps);
+    //float horStepSize = 2 * M_PI / numHorSteps;
+    //float vertStepSize = M_PI / (2 * numVertSteps);
+    float horStepSize = M_PI / numHorSteps;
+    float vertStepSize = M_PI / (numVertSteps);
 
     int vertexCounter = 0;
     for (int i=0; i<numHorSteps; i++) {
         float horizontalAngle = horStepSize * i;
         for (int j=0; j<=numVertSteps; j++) {
             float verticalAngle = vertStepSize * j;
-            float x = radius * cosf(horizontalAngle) * sinf(2 * verticalAngle);
-            float y = radius * sinf(horizontalAngle) * sinf(2 * verticalAngle);
-            float z = radius * (pow(cosf(verticalAngle), 2) - pow(cosf(horizontalAngle), 2) * pow(sinf(verticalAngle), 2));
+            //float x = radius * cosf(horizontalAngle) * sinf(2 * verticalAngle);
+            //float y = radius * sinf(horizontalAngle) * sinf(2 * verticalAngle);
+            //float z = radius * (pow(cosf(verticalAngle), 2) - pow(cosf(horizontalAngle), 2) * pow(sinf(verticalAngle), 2));
+
+            //float x = radius * pow(cosf(horizontalAngle), 2) * sinf(2 * verticalAngle);
+            //float y = radius * sinf(2 * horizontalAngle) * cosf(verticalAngle);
+            //float z = (-1) * radius * pow(cosf(horizontalAngle), 2) * cosf(2 * verticalAngle);
+
+            //float x = radius * sinf(horizontalAngle) * sinf(2 * verticalAngle);
+            //float y = radius * sinf(2 * horizontalAngle) * pow(sinf(verticalAngle), 2);
+            //float z = radius * cosf(2 * horizontalAngle) * pow(sinf(verticalAngle), 2);
+
+            float x = radius * sinf(2 * horizontalAngle) * sinf(verticalAngle);
+            float y = radius * pow(cosf(horizontalAngle), 2) * sinf(2 * verticalAngle);
+            float z = radius * (pow(sinf(horizontalAngle), 2) - pow(cosf(horizontalAngle), 2) * pow(sinf(verticalAngle), 2));
 
             vertices[vertexCounter] = {QVector3D(x,  y,  z)};
             vertexCounter += 1;
